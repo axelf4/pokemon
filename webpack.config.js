a920d974dc2912143d504faa7533523d411b83d5
Axel Forsman <axelsfor@gmail.com>
2018-05-17 09:59:22 +0200

    Update webpack to latest version.

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const path = require('path');
 module.exports = {
 	entry: ["babel-polyfill", "./src/main.js"],
 	resolve: {
-		root: [
-			path.resolve('./src'),
-			path.resolve('./glsl'),
-		],
+		modules: [
+			path.join(__dirname, "src"),
+			path.join(__dirname, "glsl"),
+			"node_modules",
+		]
 	},
 	output: {
 		// path: path.join(__dirname, 'build'),
@@ -15,17 +16,22 @@ module.exports = {
 	},
 	devtool: "source-map",
 	module: {
-		loaders: [
+		rules: [
 		{
 			test: /\.js$/,
 			exclude: /(node_modules|bower_components)/,
-			loader: "babel",
-			query: { presets: ["es2015"] }
+			loader: "babel-loader",
+			options: {
+				presets: ["env"]
+			}
 		},
-		{ test: /\.glsl$/, loader: "webpack-glsl" },
-		{ test: /BitSetModule/, loader: "exports?BitSetModule" },
-		{ test: /fowl/, loader: "imports?BitsetModule=BitSetModule!exports?fowl" },
-		{ test: /flex\.js/, loader: "exports?flex" },
+		{ test: /\.glsl$/, loader: "webpack-glsl-loader" },
+		{ test: /BitSetModule/, loader: "exports-loader?BitSetModule" },
+		{
+			test: /fowl/,
+			loader: "imports-loader?BitsetModule=BitSetModule!exports-loader?fowl"
+		},
+		{ test: /flex\.js/, loader: "exports-loader?flex" },
 		]
 	}
 };
