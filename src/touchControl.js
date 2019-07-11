c2df10a460063207cf2cf969fcf400fae2953b05
Axel Forsman <axelsfor@gmail.com>
2019-07-11 13:44:28 +0200

    Add A/B touch screen buttons

--- src/touchControl.js
+++ src/touchControl.js
@@ -14,12 +14,24 @@ function attach() {
 			#controls {
 				position: absolute;
 				display: flex;
-				top: 0;
-				right: 0;
-				bottom: 20px;
-				left: 20px;
-				align-items: flex-end;
+				right: 0; bottom: 0; left: 0;
+				margin: 5vmin;
+				justify-content: space-between; align-items: flex-end;
+				opacity: 0.75;
+				color: #212121;
+				font-family: "open sans", sans-serif;
+
 				user-select: none;
+				-moz-user-select: none;
+			}
+
+			#controls *[data-key] {
+				background: #333333;
+				transition: all 0.5s cubic-bezier(.25, 1.7, .35, .8);
+			}
+
+			#controls *[data-key]:active {
+				filter: brightness(2) drop-shadow(0 0 20px black);
 			}
 
 			#dpad {
@@ -28,18 +40,13 @@ function attach() {
 				grid-template-columns: repeat(3, 1fr);
 				grid-auto-rows: auto;
 				width: 45vmin; height: 45vmin;
-				margin: 5vh;
+				margin: 5vmin;
 			}
 
 			#dpad > * {
 				position: relative;
-				/* width: 5vw; height: 10vh; */
 				margin-top: 35%;
-
-				background: #333333;
-				opacity: 0.75;
-				border-radius: 0 0 5px 5px;
-				transition: all 0.5s cubic-bezier(.25, 1.7, .35, .8);
+				border-radius: 0 0 15% 15%;
 			}
 
 			#dpad > *:after {
@@ -48,45 +55,54 @@ function attach() {
 				bottom: 100%;
 				left: 0; right: 0;
 				padding-bottom: 35%;
-				background: #333333;
+				background: inherit;
 				clip-path: polygon(0% 100%, 50% 0%, 100% 100%);
-			}
-
-			#dpad > *:active {
-				filter: brightness(2) drop-shadow(0 0 20px black);
 			}
 
 			#up { grid-column: 2; grid-row: 1; }
 			#left { transform: rotate(270deg); grid-column: 1; grid-row: 2; }
 			#right { transform: rotate(90deg); grid-column: 3; grid-row: 2; }
 			#down { transform: rotate(180deg); grid-column: 2; grid-row: 3; }
+
+			.action-buttons {
+				position: relative;
+				width: 45vmin;
+				margin-right: 5vmin;
+				align-self: stretch;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				font-size: xx-large; font-weight: 700;
+			}
+
+			.touch-button {
+				border-radius: 50%; /* Make round */
+				width: 20vmin; height: 20vmin;
+				display: flex; justify-content: center; align-items: center;
+			}
+
+			.action-buttons > #aButton { align-self: flex-end; }
+			.action-buttons > #bButton { align-self: flex-start; }
 		</style>
 		<div id="dpad" draggable="false">
-			<div id="up"></div>
-			<div id="left"></div>
-			<div id="right"></div>
-			<div id="down"></div>
+			<div id="up" data-key="w"></div>
+			<div id="left" data-key="a"></div>
+			<div id="right" data-key="d"></div>
+			<div id="down" data-key="s"></div>
+		</div>
+		<div class="action-buttons">
+			<div id="aButton" class="touch-button" data-key=" ">A</div>
+			<div id="bButton" class="touch-button" data-key="Shift">B</div>
 		</div>
 	`;
 	controls.id = "controls";
-	document.body.appendChild(controls);
-
-	let dpadElement = document.getElementById("dpad");
 	for (let [eventType, keyEvent] of [["mousedown", "keydown"], ["mouseup", "keyup"], ["mouseout", "keyup"]]) {
-		dpadElement.addEventListener(eventType, event => {
-			if (!event.target) return;
-			let key = null;
-			switch (event.target.id) {
-				case "up": key = "w"; break;
-				case "left": key = "a"; break;
-				case "down": key = "s"; break;
-				case "right": key = "d"; break;
-			}
-			if (key === null) return;
-
-			window.dispatchEvent(new KeyboardEvent(keyEvent, { key }));
+		controls.addEventListener(eventType, event => {
+			if (!(event.target && "key" in event.target.dataset)) return;
+			window.dispatchEvent(new KeyboardEvent(keyEvent, { key: event.target.dataset.key }));
 		});
 	}
+	document.body.appendChild(controls);
 }
 
 if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", attach);
